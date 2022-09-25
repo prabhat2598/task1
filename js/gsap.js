27aec234e42ab393ac3d22c8c587145efc6a20c8
prabhat <pbarman@netlink.com>
2022-09-26 02:08:53 +0530

    some fix

---
+++ js/gsap.js
@@ -0,0 +1,46 @@
+gsap.from("#botl", {
+    y: 500,
+    delay:0.5,
+    opacity: 0
+
+})
+gsap.from("h5", {
+    y: 200,
+    delay: 0.5,
+    opacity: 0
+})
+gsap.from(".pTag", {
+    y: 200,
+    delay: 0.5,
+    opacity: 0
+
+})
+gsap.from(".photo2", {
+    y: 200,
+    delay: 2,
+    opacity: 0
+
+})
+gsap.from(".heading1", {
+    y: 200,
+    opacity: 1,
+    delay: 2,
+    opacity: 0
+})
+gsap.from(".heading2", {
+    y: 200, opacity: 1,
+    delay: 2,
+    opacity: 0
+})
+
+gsap.from(".buttonPage3", {
+    y: 200,
+    opacity: 1,
+    delay: 2,
+    opacity: 0
+})
+gsap.from(".rightHalf", {
+    y: 200,
+    opacity: 1,
+    opacity: 0
+})
